a69200d02619a89f2db55e7bbab4db2935b75604
Danny Nuch <dannynuch@gmail.com>
2020-02-03 17:56:45 -0800

    Add switch_led module

--- projects/lpc40xx_freertos/l5_application/periodics/test/test_periodic_callbacks.c
+++ projects/lpc40xx_freertos/l5_application/periodics/test/test_periodic_callbacks.c
@@ -6,8 +6,13 @@
 // Include the Mocks
 // - This will not pull the REAL source code of these modules (such as board_io.c)
 // - This will auto-generate "Mock" versions based on the header file
+
 #include "Mockboard_io.h"
 #include "Mockgpio.h"
+//#include "MockFreeRTOS.h"
+//#include "Mocktask.h"
+
+#include "Mockswitch_led_logic.h"
 
 // Include the source we wish to test
 #include "periodic_callbacks.h"
@@ -16,11 +21,18 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void test__periodic_callbacks__initialize(void) { periodic_callbacks__initialize(); }
+void test__periodic_callbacks__initialize(void) {
+  switch_led_logic__initialize_Expect();
+
+  periodic_callbacks__initialize();
+}
 
 void test__periodic_callbacks__1Hz(void) {
   gpio_s gpio = {};
   board_io__get_led0_ExpectAndReturn(gpio);
   gpio__toggle_Expect(gpio);
+
+  switch_led_logic__run_once_Expect();
+  // vTaskDelay_Expect(1000);
   periodic_callbacks__1Hz(0);
 }
