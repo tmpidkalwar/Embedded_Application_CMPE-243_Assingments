6ed3e2a8d8efe8ffdd5f35a4983343910a335157
Hari <kurahariharan@gmail.com>
2020-02-06 16:00:34 -0800

    Lab1 updates

--- projects/lpc40xx_freertos/l5_application/periodics/test/test_periodic_callbacks.c
+++ projects/lpc40xx_freertos/l5_application/periodics/test/test_periodic_callbacks.c
@@ -8,19 +8,26 @@
 // - This will auto-generate "Mock" versions based on the header file
 #include "Mockboard_io.h"
 #include "Mockgpio.h"
+#include "Mocktask.h"
 
 // Include the source we wish to test
+#include "Mockswitch_led_logic.h"
 #include "periodic_callbacks.h"
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test__periodic_callbacks__initialize(void) { periodic_callbacks__initialize(); }
+void test__periodic_callbacks__initialize(void) {
+  switch_led_logic__initialize_Expect();
+  periodic_callbacks__initialize();
+}
 
 void test__periodic_callbacks__1Hz(void) {
   gpio_s gpio = {};
   board_io__get_led0_ExpectAndReturn(gpio);
   gpio__toggle_Expect(gpio);
+
+  // switch_led_logic__run_once_Expect();
   periodic_callbacks__1Hz(0);
 }
