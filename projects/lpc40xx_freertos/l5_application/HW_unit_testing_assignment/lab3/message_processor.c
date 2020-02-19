6151b611d382638241100a67af9c126fc0f5304c
Ashish Ladage <ladageashish@gmail.com>
2020-02-19 02:28:12 -0800

    changes in lab 3

--- projects/lpc40xx_freertos/l5_application/HW_unit_testing_assignment/lab3/message_processor.c
+++ projects/lpc40xx_freertos/l5_application/HW_unit_testing_assignment/lab3/message_processor.c
@@ -9,37 +9,31 @@
  *   - There are no messages to process -- which happens when message__read() returns false
  *   - At most 3 messages have been read
  */
+static bool third_message(message_s *mes) {
+  bool symbol_found = false;
+  if (mes->data[0] == '$') {
+    mes->data[1] = '%';
+    symbol_found = true;
+  } else if (mes->data[0] == '#') {
+    mes->data[1] = '@';
+    symbol_found = true;
+  } else {
+  }
+  return symbol_found;
+}
 
-const static size_t max_messages_to_process = 3;
 bool message_processor(void) {
   message_s message;
   memset(&message, 0, sizeof(message));
+
+  const static size_t max_messages_to_process = 3;
   for (size_t message_count = 0; message_count < max_messages_to_process; message_count++) {
     if (!message__read(&message)) {
-      return false;
       break;
-    }
-  }
-}
-bool process_message_with_dollar_sign(void) {
-  message_s message;
-  for (size_t message_count = 0; message_count < max_messages_to_process; message_count++) {
-    if (message.data[0] == '$') {
-      return true;
-      break;
+    } else if (message_count < 2) {
+
     } else {
-      return false;
-    }
-  }
-}
-bool process_messages_without_any_dollar_sign(void) {
-  message_s message;
-  for (size_t message_count = 0; message_count < max_messages_to_process; message_count++) {
-    if (message.data[0] != '$') {
-      return true;
-      break;
-    } else {
-      return false;
+      third_message(&message);
     }
   }
 }
--- projects/lpc40xx_freertos/l5_application/periodics/test/test_message_processor.c
+++ projects/lpc40xx_freertos/l5_application/periodics/test/test_message_processor.c
@@ -19,16 +19,51 @@ void test_process_3_messages(void) {
   TEST_ASSERT_FALSE(message_processor());
 }
 
-void test_process_message_with_dollar_sign(void) {
-  message__read_ExpectAndReturn('!', true);
-  message__read_IgnoreArg_message_to_read();
-  TEST_ASSERT_FALSE(process_message_with_dollar_sign());
-
-  // Since we did not return a message that starts with '$' this should return false
+void test_third_msg(void) {
+  message_s m;
+  m.data[0] = '$';
+  m.data[1] = '%';
+  TEST_ASSERT_TRUE(third_message(&m));
+  // m.data[0] = '#';
+  // TEST_ASSERT_TRUE(third_message());
 }
 
-void test_process_messages_without_any_dollar_sign(void) {
-  message__read_ExpectAndReturn('$', true);
+void test_process_message_with_dollar_sign(void) {
+  message_s m;
+  m.data[0] = '$';
+
+  message__read_ExpectAndReturn(NULL, true);
   message__read_IgnoreArg_message_to_read();
-  TEST_ASSERT_FALSE(process_message_with_dollar_sign());
+  message__read_ReturnThruPtr_message_to_read(&m);
+
+  message__read_ExpectAndReturn(NULL, true);
+  message__read_IgnoreArg_message_to_read();
+  message__read_ReturnThruPtr_message_to_read(&m);
+
+  message__read_ExpectAndReturn(NULL, true);
+  message__read_IgnoreArg_message_to_read();
+  message__read_ReturnThruPtr_message_to_read(&m);
+
+  TEST_ASSERT_TRUE(message_processor());
+}
+
+// Since we did not return a message that starts with '$' this should return false
+
+void test_process_messages_without_any_dollar_sign(void) {
+  message_s m;
+  m.data[0] = 'c';
+
+  message__read_ExpectAndReturn(NULL, true);
+  message__read_IgnoreArg_message_to_read();
+  message__read_ReturnThruPtr_message_to_read(&m);
+
+  message__read_ExpectAndReturn(NULL, true);
+  message__read_IgnoreArg_message_to_read();
+  message__read_ReturnThruPtr_message_to_read(&m);
+
+  message__read_ExpectAndReturn(NULL, true);
+  message__read_IgnoreArg_message_to_read();
+  message__read_ReturnThruPtr_message_to_read(&m);
+
+  TEST_ASSERT_FALSE(message_processor());
 }
