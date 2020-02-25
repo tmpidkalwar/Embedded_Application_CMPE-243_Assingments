f87bb47f8329812cf764922637aa4e29e6e15d9b
preet <pkang@sibros.tech>
2020-02-25 10:57:34 -0800

    closes #72_codegen: Generate structs, need to handle mux

--- site_scons/site_tools/codegen/dbc_to_c.py
+++ site_scons/site_tools/codegen/dbc_to_c.py
@@ -17,6 +17,74 @@ def get_args():
     return arg_parser.parse_args()
 
 
+class code_writer:
+    def __init__(self, stream):
+        self._stream = stream
+
+    def file_header(self, dbc_filename):
+        self._stream.write('''
+// AUTO-GENERATED - DO NOT EDIT
+// Generated from {0}
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+'''.format(dbc_filename))
+
+    def mia_struct(self):
+        self._stream.write('''
+/// Missing in Action (MIA) structure
+typedef struct {
+  uint32_t is_mia : 1;          ///< Missing in action flag
+  uint32_t mia_counter_ms : 31; ///< Missing in action counter
+} dbc_mia_info_t;
+''')
+
+    def structs(self, messages, generate_layout=False):
+        # https://cantools.readthedocs.io/en/latest/#cantools.database.can.Message
+        for message in messages:
+            message_layout = ("\n"+message.layout_string() if generate_layout else "")
+            signal_members = self._gen_struct_signals(message)
+
+            self._stream.write('''
+/**
+ * {0}: Sent by {1} {2}
+ */
+typedef struct {{
+{3}
+}} dbc_{4}_s;
+'''.format(message.name, message.senders[0], message_layout, signal_members, message.name))
+
+    def _gen_struct_signals(self, message):
+        signals_string = ""
+        for signal in message.signals:
+            type_and_name = "{0} {1}".format(self._get_signal_type(signal), signal.name).ljust(40)
+            signals_string += "  {0}; // Scale={1}\n".format(type_and_name, signal.scale)
+
+        return signals_string
+
+    def _get_signal_type(self, signal):
+        t = "float";
+
+        if (signal.scale * 1.0).is_integer():
+            _max = (2 ** signal.length) * signal.scale
+            if signal.is_signed:
+                _max *= 2
+
+            t = "uint32_t"
+            if _max <= 256:
+                t = "uint8_t"
+            elif _max <= 65536:
+                t = "uint16_t"
+
+            # If the signal is signed, or the offset is negative, remove "u" to use "int" type.
+            if signal.is_signed or signal.offset < 0:
+                t = t[1:]
+
+        return t
+
+
 def main():
     # args = get_args()
     # dbc_filepath = args.dbc
@@ -26,11 +94,17 @@ def main():
         print("Unable to find DBC file: [{}]".format(dbc_filepath))
 
     dbc = cantools.database.load_file(dbc_filepath)
+    cw = code_writer(sys.stdout)
+    cw.file_header(dbc_filepath)
+    #cw.mia_struct()
+    cw.structs(dbc.messages)
+    return
 
     for message in dbc.messages:
-        print(message)  # https://cantools.readthedocs.io/en/latest/#cantools.database.can.Message
+        print(message)  #
         for signal in message.signals:
             print(signal)  # https://cantools.readthedocs.io/en/latest/#cantools.database.can.Signal
+
 
 if __name__ == "__main__":
     sys.exit(main())
