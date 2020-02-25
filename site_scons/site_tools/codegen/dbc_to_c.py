2bd56d9b6aa551fc0d76ec118ad594cbd02dff0d
Jason Tran <jtran@sibros.tech>
2020-02-24 18:14:57 -0800

    closes #72: temp

--- site_scons/site_tools/codegen/dbc_to_c.py
+++ site_scons/site_tools/codegen/dbc_to_c.py
@@ -18,14 +18,19 @@ def get_args():
 
 
 def main():
-    args = get_args()
-    dbc_filepath = args.dbc
+    # args = get_args()
+    # dbc_filepath = args.dbc
+    dbc_filepath = os.path.join(os.path.dirname(__file__), "tests/test.dbc")
 
     if not os.path.isfile(dbc_filepath):
         print("Unable to find DBC file: [{}]".format(dbc_filepath))
 
     dbc = cantools.database.load_file(dbc_filepath)
 
+    for message in dbc.messages:
+        print(message)  # https://cantools.readthedocs.io/en/latest/#cantools.database.can.Message
+        for signal in message.signals:
+            print(signal)  # https://cantools.readthedocs.io/en/latest/#cantools.database.can.Signal
 
 if __name__ == "__main__":
     sys.exit(main())
