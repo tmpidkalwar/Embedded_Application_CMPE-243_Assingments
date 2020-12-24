b775886f1ecb983a33c06c36bfb366494a7cdd09
ychen928 <yuyuchen92893@gmail.com>
2020-12-24 01:24:40 -0800

    closes #106 address comment, fix json format, support enum type and bitmaps

--- site_scons/site_tools/symbol_table/__init__.py
+++ site_scons/site_tools/symbol_table/__init__.py
@@ -1,8 +1,7 @@
 from SCons.Script import *
 
 import os
-import subprocess
-import sys
+
 
 SELF_DIR = Dir(os.path.dirname(__file__))
 GENERATE_SYMBOL_TABLE_PY = SELF_DIR.File("generate_symbol_table.py")
@@ -23,19 +22,18 @@ def exists(env):
 
 def symbol_table_generator_method(env, source, target):
     target = Dir(target)
-    elf_filenode = File(source)
+    elf_filenode = File(source[0])
 
     basename, _ = os.path.splitext(elf_filenode.name)
     output_filenodes = []
-    output_filenodes.append(target.File("{}.{}".format(basename, "json")))
-    output_filenodes.append(target.File("{}.{}".format(basename,"log")))
+    output_filenodes.append(target.File("{}_symbol_table.{}".format(basename, "json")))
+    output_filenodes.append(target.File("{}_symbol_table.{}".format(basename, "log")))
 
     command = [
         "python",
         GENERATE_SYMBOL_TABLE_PY.abspath,
-        "--elf=$SOURCE",
-        "--output=${TARGETS[0]}",
-        "--log-output=${TARGETS[1]}",
+        "$SOURCE",
+        "--output $TARGETS",
     ]
 
     command = " ".join(command)
