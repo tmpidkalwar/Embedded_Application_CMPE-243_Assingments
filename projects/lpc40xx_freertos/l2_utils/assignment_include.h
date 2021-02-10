72393e3c57feeab7e20af534b18921e7b8be5808
Tejas Pidkalwar <tejas.pidkalwar@gmail.com>
2021-02-09 16:08:32 -0800

    Assignment include header file is added

---
+++ projects/lpc40xx_freertos/l2_utils/assignment_include.h
@@ -0,0 +1,8 @@
+/**
+ * @file assignment_include.h
+ * @author Tejas Pidkalwar
+ * @date 9th Feb 2021
+ * @brief This file maintains the macro defined to enable specific assignment implementation for
+ *      execution, along with subparts of assignment enabling feature
+ */
+
